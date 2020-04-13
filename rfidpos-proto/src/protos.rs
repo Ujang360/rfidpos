de09c183471c5ae83ce1b0700fd00a0c582b202a
Aditya Kresna <aditya.kresna@outlook.co.id>
2020-04-13 21:44:04 +0700

    Add Python package wrapper

--- rfidpos-proto/src/protos.rs
+++ rfidpos-proto/src/protos.rs
@@ -6,23 +6,24 @@ pub const PROTO_HEADER: u16 = 0xBEEF;
 pub const PROTO_LENGTH: usize = 8;
 pub const CRC16_OFFSET: usize = 6;
 
-const FLAG_ACCESS: u16 = 0x8000;
-const FLAG_WRITE: u16 = 0x4000;
-const MASK_RANK: u16 = 0b11;
-const MASK_REGISTER: u16 = 0b1111_1111_1111;
-const LSHIFT_RANK: usize = 12;
+const FLAG_ACCESS: i32 = -2_147_483_648_i32;
+const FLAG_WRITE: i32 = 0x4000_0000;
+const MASK_RANK: i32 = 0b11;
+const MASK_REGISTER: i32 = 0b1111_1111_1111;
+const MASK_DATA: i32 = 0xFFFF;
+const BIT_SHIFT_RANK: usize = 28;
+const BIT_SHIFT_REGISTER: usize = 16;
 
-#[repr(C, packed(8))]
+#[repr(C, packed(1))]
 struct ProtoFrame {
     header: u16,
-    segment0: u16,
-    segment1: u16,
+    payload: i32,
     crc16_genibus: u16,
 }
 
-#[repr(C, packed(8))]
+#[repr(C, packed(1))]
 pub union ProtoUnion {
-    raw_frame: [u8; PROTO_LENGTH],
+    pub(crate) raw_frame: [u8; PROTO_LENGTH],
     data_frame: ProtoFrame,
 }
 
@@ -30,45 +31,70 @@ impl ProtoFrame {
     fn new() -> ProtoFrame {
         ProtoFrame {
             header: PROTO_HEADER,
-            segment0: 0,
-            segment1: 0,
+            payload: 0,
             crc16_genibus: 0xE38D,
         }
     }
 }
 
 impl ProtoUnion {
-    pub fn update_crc16(&mut self) {
+    fn update_crc16(&mut self) {
         unsafe {
-            self.data_frame.crc16_genibus =
-                State::<GENIBUS>::calculate(&(&self.raw_frame)[0..(CRC16_OFFSET - 1)]);
+            self.data_frame.crc16_genibus = self.compute_crc16();
         }
+    }
+
+    fn compute_crc16(&self) -> u16 {
+        unsafe { State::<GENIBUS>::calculate(&(&self.raw_frame)[0..(CRC16_OFFSET - 1)]) }
+    }
+
+    pub fn update_from_raw_bytes(&mut self, raw_bytes: &[u8]) -> Result<(), String> {
+        unsafe {
+            self.raw_frame[..].clone_from_slice(&raw_bytes[..]);
+
+            if self.compute_crc16() != self.data_frame.crc16_genibus {
+                return Err("Invalid CRC-16, ensure remote calculate CRC-16 using GENIBUS standard!".into());
+            }
+        }
+
+        Ok(())
     }
 
     pub fn borrow_raw_frame(&self) -> Arc<&[u8; PROTO_LENGTH]> {
         unsafe { Arc::new(&self.raw_frame) }
     }
 
-    pub fn fill_with_empty_frame(&mut self, is_write_mode: bool, rank: u16, register: u16) {
+    pub fn fill_with_empty_frame(&mut self, is_write_mode: bool, rank: i32, register: i32) {
         self.fill(is_write_mode, rank, register, 0);
     }
 
-    pub fn fill(&mut self, is_write_mode: bool, rank: u16, register: u16, data: u16) {
+    pub fn fill(&mut self, is_write_mode: bool, rank: i32, register: i32, data: i32) {
         unsafe {
-            self.data_frame.segment0 = 0;
-            self.data_frame.segment0 |= (rank & MASK_RANK) << LSHIFT_RANK;
-            self.data_frame.segment0 |= register & MASK_REGISTER;
-            self.data_frame.segment1 = data;
+            self.data_frame.payload = data & MASK_DATA;
+            self.data_frame.payload |= (rank & MASK_RANK) << BIT_SHIFT_RANK;
+            self.data_frame.payload |= (register & MASK_REGISTER) << BIT_SHIFT_REGISTER;
 
             if data != 0 {
-                self.data_frame.segment0 |= FLAG_ACCESS;
+                self.data_frame.payload |= FLAG_ACCESS;
             }
 
             if is_write_mode {
-                self.data_frame.segment0 |= FLAG_WRITE;
+                self.data_frame.payload |= FLAG_WRITE;
             }
         }
         self.update_crc16();
+    }
+
+    pub fn parse_info(&self) -> (bool, bool, i32, i32, i32) {
+        unsafe {
+            let is_access = self.data_frame.payload & FLAG_ACCESS != 0;
+            let is_write = self.data_frame.payload & FLAG_WRITE != 0;
+            let rank = (self.data_frame.payload >> BIT_SHIFT_RANK) & MASK_RANK;
+            let register = (self.data_frame.payload >> BIT_SHIFT_REGISTER) & MASK_RANK;
+            let data = self.data_frame.payload & MASK_DATA;
+
+            (is_access, is_write, rank, register, data)
+        }
     }
 }
 
--- rfidpos-tester/src/main.rs
+++ rfidpos-tester/src/main.rs
@@ -1,8 +1,12 @@
+use crc16::{State, GENIBUS};
 use rfidpos_proto::ProtoUnion;
 
 fn main() {
     let mut proto_instance = ProtoUnion::default();
     println!("{:02X?}", proto_instance.borrow_raw_frame());
-    proto_instance.fill_with_empty_frame(true, 0, 0xEC);
+    proto_instance.fill_with_empty_frame(true, 0, 0xB0);
     println!("{:02X?}", proto_instance.borrow_raw_frame());
+    let sample_data = vec![0xC1, 0x00, 0xF0];
+    let crc16_genibus = State::<GENIBUS>::calculate(&sample_data);
+    println!("{:#04X}", crc16_genibus);
 }
